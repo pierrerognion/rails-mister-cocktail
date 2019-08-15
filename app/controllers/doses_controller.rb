3d5e76a59d17f218b7dda3f5bd00c3989ff61639
Pierre Rognion <pierrerognion@gmail.com>
2019-08-15 15:03:31 +0200

    progress

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,47 @@
+class DosesController < ApplicationController
+  def index
+    @doses = Dose.all
+  end
+
+  def show
+    @dose = Dose.find(params[:id])
+  end
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @dose = Dose.find(params[:id])
+  end
+
+  def update
+    @doses = Dose.find(params[:id])
+    if @dose.update(dose_params)
+      redirect_to dose_path(@dose)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+    redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails
+  resources :cocktails do
+    resources :doses do
+      resources :ingredients
+    end
+  end
 end
