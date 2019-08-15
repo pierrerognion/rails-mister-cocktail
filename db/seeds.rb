20d42c75e9fa93ad145eb5a37a63a72a5bc0842a
Pierre Rognion <pierrerognion@gmail.com>
2019-08-15 12:54:11 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Ingredient.destroy_all
+
+puts 'Creating 3 ingredients...'
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "rhum")
+
+puts 'Finished!'
